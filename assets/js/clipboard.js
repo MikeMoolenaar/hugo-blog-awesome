f2f5164a23088e5006c0cfc27fb1c49edfcd6a5b
MikeMoolenaar <heeylego@gmail.com>
2023-03-22 22:03:24 +0100

    Update clipboard styling with FontAwesome icons

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,27 +1,40 @@
 // Based on: https://logfetch.com/hugo-add-copy-to-clipboard-button/
 const svgCopy =
-  '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true"><path fill-rule="evenodd" d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"></path><path fill-rule="evenodd" d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"></path></svg>';
+  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+  <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+  <path d="M224 0c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224zM64 160c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64H64V224h64V160H64z"/>
+  </svg>`
 const svgCheck =
-  '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true"><path fill-rule="evenodd" fill="rgb(63, 185, 80)" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>';
+  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+  <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+  <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
+  </svg>`
 
 const addCopyButtons = (clipboard) => {
   // 1. Look for pre > code elements in the DOM
   document.querySelectorAll("pre > code").forEach((codeBlock) => {
+    if (!codeBlock.className.includes("language-")) return;
     // 2. Create a button that will trigger a copy operation
-    const button = document.createElement("button");
-    button.className = "clipboard-button";
-    button.type = "button";
+    const button = document.createElement("div");
+    button.className = "clipboard-container";
     button.innerHTML = svgCopy;
     button.title = "Copy to clipboard";
 
     const code = codeBlock.innerText.replace(/\n\n/g, '\n');
 
+    const removeSvg = () => {
+      const existingSvg = button.querySelector("svg");
+      if (existingSvg) existingSvg.remove();
+    }
+
     button.addEventListener("click", () => {
       clipboard.writeText(code).then(
         () => {
-          button.blur();
           button.innerHTML = svgCheck;
-          setTimeout(() => (button.innerHTML = svgCopy), 2000);
+
+          setTimeout(() => { 
+            button.innerHTML = svgCopy;
+           }, 2000);
         },
         (error) => (button.innerHTML = "Error")
       );
